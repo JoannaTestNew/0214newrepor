4c9f34b8211828351306fddd277087b954e58f3b
Joanna Zheng (BEYONDSOFT CONSULTING INC) <v-joazh@microsoft.com>
2026-02-14 12:37:19 +0800

    #1 sdfsdf

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,6 +4,6 @@
     {
         /// <summary>aaaaa
         /// add a method that uses a feature from C# 14.0, such as lambda expressions with inferred return types
-        /// aaaaaaaasdfsdfsdfsdfsdf
+        /// aaaaaaaasdfsdfsdfsdfsdfsdfsdf
     }
 }
