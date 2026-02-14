80feb8cbd0afa436fc698e9ade5ad6b1c5fa5f87
Joanna Zheng (BEYONDSOFT CONSULTING INC) <v-joazh@microsoft.com>
2026-02-14 12:09:11 +0800

    commit to all repos

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,7 +2,7 @@
 {
     public class Class1
     {
-        /// <summary>
+        /// <summary>aaaaa
         /// add a method that uses a feature from C# 14.0, such as lambda expressions with inferred return types
         /// aaaaaaaasdfsdfsdfsdfsdf
     }
